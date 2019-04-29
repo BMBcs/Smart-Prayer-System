3b4adb0cc269daf14f6fd861a03d24cd83fb123f
BMBcs <48153156+BMBcs@users.noreply.github.com>
2019-04-29 15:16:58 +0300

    Trying to display Arabic character on the LCD

--- Embedded_System_Software/Embedded_System_Software.ino
+++ Embedded_System_Software/Embedded_System_Software.ino
@@ -33,9 +33,9 @@ LiquidCrystal_I2C lcd (i2cAddress, en, rw, rs, d4, d5, d6, d7, bl, POSITIVE);
 
 
 ///////// setup the ultrasonic sensor 
-#define trigPin  8
-#define echoPin  9
-#define MAX_DISTANCE  20
+#define trigPin  12
+#define echoPin  11
+#define MAX_DISTANCE  200
 NewPing disSensor(trigPin, echoPin, MAX_DISTANCE);
 
 
@@ -81,8 +81,273 @@ bool sittingCounter_start = true;
 char recieved_Data;
 
 
+//////////////// a bunch of bytes represent the arabic characters ///////////////////////
 
+byte m[8]={ // م
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00000,
+  0b11111,
+  0b00101,
+  0b00111
+};
 
+byte r[8]={ // ر
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00010,
+  0b00010,
+  0b00100,
+  0b01000,
+  0b10000
+};
+
+byte h[8]={ //ح متصل
+  0b00000,
+  0b00000,
+  0b00000,
+  0b11100,
+  0b00010,
+  0b11111,
+  0b00000,
+  0b00000
+};
+
+byte b[8]={ // ب متصله
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00100,
+  0b00100,
+  0b11111,
+  0b00000,
+  0b00100
+};
+
+byte a3[8]={ // الف متطرفة
+  0b00000,
+  0b00000,
+  0b00100,
+  0b00100,
+  0b00100,
+  0b00111,
+  0b00000,
+  0b00000
+};
+
+byte a1[8]={ // الف
+  0b00000,
+  0b00100,
+  0b00100,
+  0b00100,
+  0b00100,
+  0b00100,
+  0b00000,
+  0b00000
+};
+
+byte g[8]={ // ق
+  0b00000,
+  0b00101,
+  0b00000,
+  0b01111,
+  0b01001,
+  0b11111,
+  0b00000,
+  0b00000
+};
+
+byte t2[8]={ // ت متصله
+  0b00000,
+  0b00101,
+  0b00000,
+  0b00010,
+  0b00010,
+  0b11111,
+  0b00000,
+  0b00000
+};
+
+byte n[8]={ // ن
+  0b00000,
+  0b00000,
+  0b00100,
+  0b00000,
+  0b10001,
+  0b10001,
+  0b01110,
+  0b00000
+};
+
+byte s[8]={ //س
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00000,
+  0b10101,
+  0b11111,
+  0b00000,
+  0b00000
+};
+
+byte j[8]={ //ج
+  0b00000,
+  0b00000,
+  0b00000,
+  0b11100,
+  0b00010,
+  0b11111,
+  0b00000,
+  0b01000
+};
+
+byte w[8]={ //و
+  0b00000,
+  0b00000,
+  0b01110,
+  0b10001,
+  0b01001,
+  0b00010,
+  0b00100,
+  0b01000
+};
+
+byte o3[8]={ //ع متطرفة
+  0b00000,
+  0b01110,
+  0b10000,
+  0b11110,
+  0b10000,
+  0b10000,
+  0b10000,
+  0b01110
+};
+
+byte f[8]={ //ف متصل
+  0b00000,
+  0b00010,
+  0b00000,
+  0b00111,
+  0b01001,
+  0b11111,
+  0b00000,
+  0b00000
+};
+
+byte k[8]={ //ك متصل
+  0b00001,
+  0b00100,
+  0b10000,
+  0b01000,
+  0b00010,
+  0b11111,
+  0b00000,
+  0b00000
+};
+
+byte h2[8]={ // ه متصله
+  0b00000,
+  0b00000,
+  0b00000,
+  0b01110,
+  0b01010,
+  0b11111,
+  0b01010,
+  0b01110
+};
+
+byte h3[8]={ // ه متطرفة
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00000,
+  0b11100,
+  0b10010,
+  0b11111,
+  0b00000
+};
+
+byte sh[8]={ //ش متصله
+  0b00000,
+  0b00100,
+  0b01010,
+  0b00000,
+  0b10101,
+  0b11111,
+  0b00000,
+  0b00000
+};
+
+byte o2[8]={ //ع متصله
+  0b00000,
+  0b00000,
+  0b00000,
+  0b01110,
+  0b01010,
+  0b11111,
+  0b00000,
+  0b00000
+};
+
+byte t3[8]={ // ت متطرفة
+  0b00000,
+  0b01010,
+  0b00000,
+  0b10001,
+  0b10001,
+  0b11111,
+  0b00000,
+  0b00000
+};
+
+byte o1[8]={ // ع ابتدائية
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00111,
+  0b00100,
+  0b11111,
+  0b00000,
+  0b00000
+};
+
+byte d[8]={ // د
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00010,
+  0b00010,
+  0b11111,
+  0b00000,
+  0b00000
+};
+
+byte l[8]={ //ل
+  0b00000,
+  0b00000,
+  0b00010,
+  0b00010,
+  0b00010,
+  0b11110,
+  0b00000,
+  0b00000
+};
+
+byte kh[8]={ //خ
+  0b00000,
+  0b01000,
+  0b00000,
+  0b11100,
+  0b00010,
+  0b11111,
+  0b00000,
+  0b00000
+};
+
+////////////////
 void setup() {
   Serial.begin(9600);
   BT.begin(9600);
@@ -95,17 +360,43 @@ void setup() {
   pinMode(key3, INPUT_PULLUP);
   pinMode(key4, INPUT_PULLUP);
 
-  lcd.setCursor(3,0);
-  lcd.print("welcome!!");
+  
+  
+  lcd.createChar(1,m);
+  lcd.createChar(2,r);
+  lcd.createChar(3,h);
+  lcd.createChar(4,b);
+  lcd.createChar(5,a3);
+  delay(500);
+  lcd.setCursor(4,0);
+  
+  lcd.write(5);
+  lcd.write(4);
+  lcd.write(3);
+  lcd.write(2);
+  lcd.write(1);
+  
   delay(3000);
   lcd.clear();
-  lcd.setCursor(2,0);
-  lcd.print("Pairing");
-  lcd.setCursor(0,1);
-  lcd.print("OFFline Press'B'");
-//  lcd.print("Choose Num Rakah");
-//  lcd.setCursor(3,1);
-//  lcd.print(rakahNum);
+  
+  lcd.createChar(1,a1);
+  lcd.createChar(2,g);
+  lcd.createChar(3,t2);
+  lcd.createChar(4,r);
+  lcd.createChar(5,n);
+
+  lcd.setCursor(6,0);
+  lcd.write(5);
+  lcd.write(1);
+  lcd.write(4);
+  lcd.write(3);
+  lcd.write(2);
+  lcd.write(1);
+  //lcd.print("Pairing");
+  
+  //lcd.setCursor(0,1);
+  
+  //lcd.print("OFFline Press'B'");
   attachInterrupt(digitalPinToInterrupt(key3), ISR_backButton, FALLING);
   attachInterrupt(digitalPinToInterrupt(key4), ISR_offlineButton, FALLING);
 }
@@ -132,11 +423,38 @@ void loop() {
                 lcd.setCursor(0,1);
                 lcd.print("Al-Fajer");
                 delay(4000);
+                
                 lcd.clear();
-                lcd.print("Standing");
+                lcd.createChar(1,w);
+                lcd.createChar(2,g);
+                lcd.createChar(3,f);
+                lcd.write(3);
+                lcd.write(1);
+                lcd.write(2);
+                lcd.write(1);
+                //lcd.print("Standing");
+
+                lcd.createChar(1,o1);
+                lcd.createChar(2,d);
+                lcd.createChar(3,r);
+                lcd.createChar(4,k);
+                lcd.createChar(5,o2);
+                lcd.createChar(6,a3);
+                lcd.createChar(7,t3);
                 lcd.setCursor(0,1);
-                lcd.print("Done Rakah: 0/");
                 lcd.print(rakahNum);
+                lcd.print("/0:");
+                
+                lcd.write(7);
+                lcd.write(6);
+                lcd.write(5);
+                lcd.write(4);
+                lcd.write(3);
+                lcd.write(2);
+                lcd.write(2);
+                lcd.write(1);
+//                lcd.print("Done Rakah: 0/");
+//                lcd.print(rakahNum);
                 
         break;
 
@@ -285,7 +603,7 @@ void ISR_offlineButton(){
     interrupt_check1 = true; // to activate the printing Function (after_ISR_printing()).
   }else{
     bt_Mode = true;  // to switch from offline to online
-    interrupt_check2 = true; // to activate the printing Function (ISR_online_printing()).
+    interrupt_check2 = true; // to activate the printing Function (after_ISR_printing()).
   }
   
 }
@@ -318,7 +636,36 @@ void ISR_online_printing(){
 
 void after_ISR_printing(){
   lcd.clear();
-  lcd.print("Choose Num Rakah");
+  
+  lcd.createChar(1,o1);
+  lcd.createChar(2,d);
+  lcd.createChar(3,r);
+  lcd.createChar(4,k);
+  lcd.createChar(5,o2);
+  lcd.createChar(6,a3);
+  lcd.createChar(7,t3);
+
+  lcd.setCursor(0,0);
+  lcd.write(7);
+  lcd.write(6);
+  lcd.write(5);
+  lcd.write(4);
+  lcd.write(3);
+  lcd.print(" ");
+  lcd.write(2);
+  lcd.write(2);
+  lcd.write(1);
+  lcd.print(" ");
+  lcd.createChar(1,a1);
+  lcd.createChar(2,kh);
+  lcd.createChar(3,t2);
+  lcd.createChar(4,r);
+  lcd.write(4);
+  lcd.write(3);
+  lcd.write(2);
+  lcd.write(1);
+  
+  //lcd.print("Choose Num Rakah");
   lcd.setCursor(3,1);
   lcd.print(rakahNum);
   interrupt_check1 = false;
@@ -350,7 +697,7 @@ void fourRakahPrayer(){
     fsr2 = analogRead(s2);
     fsr3 = analogRead(s3);
     distanceSensor = disSensor.ping_cm();
-    
+
   //standing
                                                                                 // for tashaood
   if(fsr1 > 100 && (standing==false) && (fsr3 < 100) && (distanceSensor == 0) && (fsr2 < 100) ){
@@ -525,7 +872,7 @@ void threeRakahPrayer(){
     fsr2 = analogRead(s2);
     fsr3 = analogRead(s3);
     distanceSensor = disSensor.ping_cm();
-    Serial.println(distanceSensor);
+
   //standing
   
   if(fsr1 > 100 && (standing==false) && (fsr3 < 100) && (distanceSensor == 0) && (fsr2 < 100) ){
@@ -639,7 +986,7 @@ void threeRakahPrayer(){
       delay(7000);
       
       if(fsr2 > 100){
-        finalTashahod = true;
+       // finalTashahod = true;
         delay(5000);
         lcd.clear();
         lcd.setCursor(2,0);
@@ -691,7 +1038,7 @@ void twoRakahPrayer(){
     fsr2 = analogRead(s2);
     fsr3 = analogRead(s3);
     distanceSensor = disSensor.ping_cm();
-    Serial.println(distanceSensor);
+
   //standing
   
   if(fsr1 > 100 && (standing==false) && (fsr3 < 100) && (distanceSensor == 0) && (fsr2 < 100) ){
@@ -822,7 +1169,7 @@ void twoRakahPrayer(){
         lcd.setCursor(3,1);
         lcd.print(rakahNum);
         bigCounter = 0;
-        BT.write("f");
+        BT.write(1);
       }
     }
 
